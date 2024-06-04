df9bbcee7d573e2dd3210cf66219a3eb54d9a6e0
efe-onal-2016400267 <efeonalll@gmail.com>
2024-06-04 18:49:21 +0200

    Guess I've found out how to do minhash and LSH

--- minhash_practices.py
+++ minhash_practices.py
@@ -29,27 +29,28 @@ from pyspark.ml.feature import MinHashLSH
 from pyspark.ml.linalg import Vectors
 from pyspark.sql.functions import col
 
-dataA = [(0, Vectors.sparse(6, [0, 1, 2], [1.0, 1.0, 1.0]),),
-         (1, Vectors.sparse(6, [2, 3, 4], [1.0, 1.0, 1.0]),),
-         (2, Vectors.sparse(6, [0, 2, 4], [1.0, 1.0, 1.0]),)]
+dataA = [(0, Vectors.sparse(10, [0, 1, 2, 7], [1.0, 1.0, 1.0, 1.0]),),
+         (1, Vectors.sparse(10, [2, 3, 4, 8], [1.0, 1.0, 1.0, 1.0]),),
+         (2, Vectors.sparse(10, [0, 2, 4, 9], [1.0, 1.0, 1.0, 1.0]),)]
 dfA = spark.createDataFrame(dataA, ["id", "features"])
 
 dfA.show(truncate=100)
 
-dataB = [(3, Vectors.sparse(6, [1, 3, 5], [1.0, 1.0, 1.0]),),
-         (4, Vectors.sparse(6, [2, 3, 5], [1.0, 1.0, 1.0]),),
-         (5, Vectors.sparse(6, [1, 2, 4], [1.0, 1.0, 1.0]),)]
+dataB = [(3, Vectors.sparse(10, [1, 3, 5, 6], [1.0, 1.0, 1.0, 1.0]),),
+         (4, Vectors.sparse(10, [2, 3, 5, 7], [1.0, 1.0, 1.0, 1.0]),),
+         (5, Vectors.sparse(10, [1, 2, 4, 8], [1.0, 1.0, 1.0, 1.0]),),
+         (5, Vectors.sparse(10, [1, 2, 4, 8], [1.0, 1.0, 1.0, 1.0]),)]
 dfB = spark.createDataFrame(dataB, ["id", "features"])
 
 dfB.show(truncate=100)
 
-key = Vectors.sparse(6, [1, 3], [1.0, 1.0])
+key = Vectors.sparse(6, [0, 1, 2, 3], [1.0, 1.0, 1.0, 1.0])
 print(key)
 
 # numHashTables: how many hash functions we're going to use. 
 # As we increase the number of hash functions, number of false negatives decrease,
 # Because if 2 sets dont fall into the same bucket for one hash function, they might for another hash function
-mh = MinHashLSH(inputCol="features", outputCol="hashes", numHashTables=2)
+mh = MinHashLSH(inputCol="features", outputCol="hashes", numHashTables=1)
 model = mh.fit(dfA)
 
 # Feature Transformation
@@ -59,13 +60,17 @@ model.transform(dfA).show(truncate=1000)
 # Compute the locality sensitive hashes for the input rows, then perform approximate
 # similarity join.
 # We could avoid computing hashes by passing in the already-transformed dataset, e.g.
-# `model.approxSimilarityJoin(transformedA, transformedB, 0.6)`
+# model.approxSimilarityJoin(transformedA, transformedB, 0.6)
+# distance = 1 - JaccardSim
 print("Approximately joining dfA and dfB on distance smaller than 0.6:")
-model.approxSimilarityJoin(dfA, dfB, 0.6, distCol="JaccardDistance")\
+similars = model.approxSimilarityJoin(dfA, dfB, 0.6, distCol="JaccardDistance")\
     .select(col("datasetA.id").alias("idA"),
             col("datasetB.id").alias("idB"),
-            col("JaccardDistance")).show()
+            col("JaccardDistance"))
+# similars = similars.withColumn("JaccardSimilarity", 1 - similars.JaccardDistance).drop("JaccardDistance")
+similars.show(truncate=100)
 
+ 
 # Compute the locality sensitive hashes for the input rows, then perform approximate nearest
 # neighbor search.
 # We could avoid computing hashes by passing in the already-transformed dataset, e.g.
@@ -74,3 +79,32 @@ model.approxSimilarityJoin(dfA, dfB, 0.6, distCol="JaccardDistance")\
 # found.
 print("Approximately searching dfA for 2 nearest neighbors of the key:")
 model.approxNearestNeighbors(dfA, key, 2).show()
+model.explainParams()
+
+
+
+my_df = dfA.union(dfB)
+mh = MinHashLSH(inputCol="features", outputCol="hashes", numHashTables=2)
+model = mh.fit(my_df)
+print("The hashed dataset where hashed values are stored in the column 'hashes':")
+model.transform(my_df).show(truncate=1000)
+
+# For a single hash function, we can get different buckets (hashes):
+# We can use multiple hash functions if we want to create more buckets with less elements.
+# We can apply this to different bands of the vectors. 
+# And if any two id happen to be in the same bucket for any of the bands, we make the actual comparison with the whole vector.
+"""
++---+--------------------------------+----------------+
+| id|                        features|          hashes|
++---+--------------------------------+----------------+
+|  0|(10,[0,1,2,7],[1.0,1.0,1.0,1.0])|[[1.05211356E8]]|
+|  1|(10,[2,3,4,8],[1.0,1.0,1.0,1.0])|[[9.82158528E8]]|
+|  2|(10,[0,2,4,9],[1.0,1.0,1.0,1.0])|[[1.05211356E8]]|
+|  3|(10,[1,3,5,6],[1.0,1.0,1.0,1.0])|[[2.59504543E8]]|
+|  4|(10,[2,3,5,7],[1.0,1.0,1.0,1.0])|[[2.59504543E8]]|
+|  5|(10,[1,2,4,8],[1.0,1.0,1.0,1.0])|[[5.43684942E8]]|
++---+--------------------------------+----------------+
+
+"""
+
+# To make the actual minhashing with the whole vector, we should increase numHashTables
