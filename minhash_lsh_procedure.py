5072354a43fe31cbfdb6092836bded57b60095db
efe-onal-2016400267 <efeonalll@gmail.com>
2024-06-07 12:46:34 +0200

    band hashes are created as 3 different columns. Nex step: we divide each buckets members to different frames and perform Jaccard similarity comparisons.

--- minhash_lsh_procedure.py
+++ minhash_lsh_procedure.py
@@ -12,7 +12,7 @@ from pyspark.sql.functions import col
 import random
 from pyspark.sql.functions import udf
 import numpy as np
-
+import xxhash
 
 ##### What this code does #####
 
@@ -92,6 +92,9 @@ model = mh.fit(df)
 signature_frame = model.transform(df)
 signature_frame.show(truncate=1000)
 
+# cache the signature_frame. we're gonna use it a lot.
+signature_frame.cache()
+
 
 # 3. Perform LSH bucketing by using xxHash library and band-ing strategy.
 
@@ -100,34 +103,57 @@ signature_frame.show(truncate=1000)
 # On the internet the only approach I could find is first creating actual lists from
     # the sparse vectors, then slicing them. So that's what I'll do now.
 
+# We use xxhash for band hashing since it is one of the fastest:
+# xxhash benchmark: https://xxhash.com/
+
 # function to convert sparse vectors to lists:
 @udf (returnType=ArrayType(FloatType()))
 def denser(sparse_vector):
     return sparse_vector.toArray().tolist()
 
-df = df.withColumn("dense_shinglings", denser(df.shinglings))
-df.show(truncate=1000)
+signature_frame = signature_frame.withColumn("dense_shinglings", denser(df.shinglings))
+signature_frame.show(truncate=1000)
 
 
+# TODO: Return a single list, anly hashes, not the tuple here.
 # function to split a list into bands:
-@udf(returnType=ArrayType(ArrayType(FloatType())))
+@udf(returnType=StructType([
+    StructField("bands", ArrayType(ArrayType(FloatType()))),
+    StructField("band_hashes", ArrayType(IntegerType()))
+]))
 def band_maker(whole_list):
     band_count = CONFIG["BandCount"]
     bands = []
-    hash_values = []
+    band_hashes = []
     band_size = m.ceil(len(whole_list) / band_count)
+
+    # use a seed with the hash function so it does determisitic hashing.
     
     if len(whole_list) > band_count:
+
         for i in range(band_count):
             start = i * band_size
             end = min(((i+1)*band_size), len(whole_list))
-            bands.append(whole_list[start:end])
+            band = whole_list[start:end]
+            bands.append(band)
+            hash_func = xxhash.xxh32(seed = 0)
+            hash_func.update(str(band).encode('utf-8'))
+            band_hashes.append(hash_func.intdigest())
 
-    return bands
+    return (bands, band_hashes)
 
-df = df.withColumn("bands", band_maker(df.dense_shinglings)).drop(df.dense_shinglings)
-df.show(truncate=1000)
+# Create band, band_hash dicts.
+signature_frame = signature_frame.withColumn("bands_hashes", band_maker(signature_frame.dense_shinglings)).drop("dense_shinglings")
 
+# use this line if you want to see the bands itself. I've already checked. Hashing works correctly.
+# df = df.withColumn("bands", df["bands_hashes"].getItem("bands"))
 
-a = [1]
-print(band_maker(a))
+# remove band parts, keep only band_hashes.
+signature_frame = signature_frame.withColumn("band_hashes", signature_frame["bands_hashes"].getItem("band_hashes")).drop("bands_hashes")
+
+# Now we make each band_hash a different column
+for i in range(CONFIG["BandCount"]):
+    signature_frame = signature_frame.withColumn(f"band_hash_{i}", signature_frame["band_hashes"][i])
+signature_frame = signature_frame.drop("band_hashes")
+
+signature_frame.show(truncate=1000)
